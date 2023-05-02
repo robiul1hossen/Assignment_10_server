5e6f736a0823f89c866ad1d1fdc807762b94504f
robiul1hossen <robim6225@gmail.com>
2023-05-02 23:41:43 +0600

    recipe data added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 const allData = require("./Data/AllData.json");
+const recipes = require("./Data/Recipes.json");
 
 app.use(cors());
 
@@ -13,6 +14,12 @@ app.get("/", (req, res) => {
 
 app.get("/all-data", (req, res) => {
   res.send(allData);
+});
+
+app.get("/all-data/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedRecipe = allData.find((recipe) => recipe.id == id);
+  res.send(selectedRecipe);
 });
 
 app.listen(port, () => {
