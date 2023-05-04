14e9489382c5571b78de2c36fd9d6259827a216b
robiul1hossen <robim6225@gmail.com>
2023-05-04 11:05:50 +0600

    vercel config added

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
-const allData = require("./Data/AllData.json");
-const recipes = require("./Data/Recipes.json");
+const recipes = require("./Data/AllData.json");
 
 app.use(cors());
 
@@ -12,13 +11,14 @@ app.get("/", (req, res) => {
   res.send("foodfiy is running");
 });
 
-app.get("/all-data", (req, res) => {
-  res.send(allData);
+app.get("/recipes", (req, res) => {
+  res.send(recipes);
 });
 
-app.get("/all-data/:id", (req, res) => {
+app.get("/recipe/:id", (req, res) => {
   const id = req.params.id;
-  const selectedRecipe = allData.find((recipe) => recipe.id == id);
+
+  const selectedRecipe = recipes.find((recipe) => recipe.id == id);
   res.send(selectedRecipe);
 });
 
